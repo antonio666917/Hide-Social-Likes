952db57e61b46569cf5c62be9831c2e065a127ec
acidtib <arubinofaux@gmail.com>
2024-02-27 16:37:05 -0700

    add instagram support

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -37,6 +37,54 @@ const facebookFeatures = () => {
   observer.observe(document.body, { childList: true, subtree: true });
 };
 
-if (locationUrl.toLowerCase().includes("facebook")) {
-  facebookFeatures();
+const instagramFeatures = () => {
+  let observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+      if (mutation.type !== "childList") {
+        continue;
+      }
+
+      let getAllLikeCountToolbars = document.querySelectorAll(
+        'a[href*="/liked_by/"]'
+      );
+
+      let getAllLikeUIToolbars = document.querySelectorAll(
+        "[aria-label='Like']"
+      );
+
+      getAllLikeCountToolbars.forEach((element) => {
+        let parentElement = element.parentElement;
+        if (parentElement) {
+          parentElement.style.display = "none";
+        }
+      });
+
+      getAllLikeUIToolbars.forEach((element) => {
+        let parentElement = element.parentElement;
+        if (parentElement) {
+          parentElement.style.display = "none";
+          let nextParentElement = parentElement.parentElement;
+          if (nextParentElement) {
+            nextParentElement.style.display = "none";
+          }
+        }
+      });
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+};
+
+const featuresMap = {
+  'facebook': facebookFeatures,
+  'instagram': instagramFeatures
+};
+
+const locationUrlLower = locationUrl.toLowerCase();
+
+for (const [site, features] of Object.entries(featuresMap)) {
+  if (locationUrlLower.includes(site)) {
+    features();
+    break; // stop searching once a match is found
+  }
 }
