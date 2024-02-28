2f1a78ba356a073373d6d89f0f8e9c534c34d71c
acidtib <arubinofaux@gmail.com>
2024-02-27 17:13:13 -0700

    add threads support

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -75,9 +75,53 @@ const instagramFeatures = () => {
   observer.observe(document.body, { childList: true, subtree: true });
 };
 
+const threadsFeatures = () => {
+  let observer = new MutationObserver((mutationsList) => {
+    for (let mutation of mutationsList) {
+      if (mutation.type !== "childList") {
+        continue;
+      }
+
+      let getAllLikeCountToolbars = []
+      // Using XPath to select all <span> elements whose text content contains "like"
+      const likeXpathExpression = "//span[contains(text(), ' like') and not(number(substring-before(text(), ' like')) = 0)]";
+      const likeSpans = document.evaluate(likeXpathExpression, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      // Loop through the snapshot of nodes and convert it into an array
+      for (let i = 0; i < likeSpans.snapshotLength; i++) {
+        getAllLikeCountToolbars.push(likeSpans.snapshotItem(i));
+      }
+
+      let getAllLikeUIToolbars = document.querySelectorAll(
+        "[aria-label='Like']"
+      );
+
+      getAllLikeCountToolbars.forEach((element) => {
+        let parentElement = element.parentElement;
+        if (parentElement) {
+          parentElement.style.display = "none";
+        }
+      });
+
+      getAllLikeUIToolbars.forEach((element) => {
+        let parentElement = element.parentElement;
+        if (parentElement) {
+          parentElement.style.display = "none";
+          let nextParentElement = parentElement.parentElement;
+          if (nextParentElement) {
+            nextParentElement.style.display = "none";
+          }
+        }
+      });
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+};
+
 const featuresMap = {
   'facebook': facebookFeatures,
-  'instagram': instagramFeatures
+  'instagram': instagramFeatures,
+  'threads': threadsFeatures
 };
 
 const locationUrlLower = locationUrl.toLowerCase();
